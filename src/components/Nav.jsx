19bf9f589af4f2ba87c920f9d3766dba4d2edb2e
Administrator. <toffly@outlook.com>
2023-11-08 18:33:09 +0700

    second change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,8 @@ import { hamburger } from "../assets/icons";
 import { navLinks } from "../constant";
 
 const Nav = () => {
-  
+  const [togglemenu, setTogglemenu] = useState(true);
+
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
@@ -23,8 +24,30 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
-            <img src={hamburger} alt="Hamburger" width={25} height={25}/>
+        <div className="hidden max-lg:flex flex-col items-end w-full">
+          <img
+            src={hamburger}
+            alt="menu"
+            width={25}
+            height={25}
+            onClick={() => setTogglemenu((prev) => !prev)}
+          />
+          {togglemenu && (
+            <div className="absolute top-20 rounded-lg bg-pale-blue p-4">
+              <ul>
+                {navLinks.map((item) => (
+                  <li key={item.label} className="py-2">
+                    <a
+                      href={item.href}
+                      className="font-montserrat leading-normal text-lg text-slate-gray"
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </header>
